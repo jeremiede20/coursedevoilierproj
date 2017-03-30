715d2d6e229e498510582198fbe56ffecf1ba546
Jérémie D <jeremie.de20@gmail.com>
2017-03-30 16:36:20 +0200

    first commit

---
+++ CourseVoiliers/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CourseVoiliers
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string Var1 = TextBoxMail.Text;
+            string Var2 = TextBoxPass.Text;
+
+            var nom = new Accueil();
+            nom.Show();
+            this.Close();
+        }
+        
+    }
+}
